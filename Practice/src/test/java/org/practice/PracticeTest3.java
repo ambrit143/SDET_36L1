edfc0ac66e134a24193d64f0e8571e7246eaf54f
AMBRIT <AMBRIT@DESKTOP-MOTNN2M>
2022-07-19 12:08:48 +0530

    i have added a class

---
+++ Practice/src/test/java/org/practice/PracticeTest3.java
@@ -0,0 +1,21 @@
+package org.practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest3 {
+	public class PracticeTest1 {
+	      @Test
+	      public void display() {
+
+	      Reporter.log("Executing the Test1",true);
+	      }
+	      @Test
+	      public void display2() {
+
+	      Reporter.log("Executing Test2",true);
+	      }
+
+	}
+
+}
